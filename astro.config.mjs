cfa25531a5febf16b9662160017dc0eec7e070b3
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-15 02:31:36 -0400

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import image from '@astrojs/image';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [preact(),image()],
+});
